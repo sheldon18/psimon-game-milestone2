de8a837978247fad9e19d0f28b6164a3e221099a
Ubuntu <ubuntu@ip-172-31-91-243.ec2.internal>
2020-03-26 09:33:36 +0000

    js added for computer vs player sequence, fonts updated, readme updated

--- README.md
+++ README.md
@@ -1,14 +1,57 @@
-         ___        ______     ____ _                 _  ___  
-        / \ \      / / ___|   / ___| | ___  _   _  __| |/ _ \ 
-       / _ \ \ /\ / /\___ \  | |   | |/ _ \| | | |/ _` | (_) |
-      / ___ \ V  V /  ___) | | |___| | (_) | |_| | (_| |\__, |
-     /_/   \_\_/\_/  |____/   \____|_|\___/ \__,_|\__,_|  /_/ 
- ----------------------------------------------------------------- 
+
+# PSIMON - The P is Silent
+
+README file for Interactive Frontend Development Milestone Project
+
+-----
+
+### Links
+
+[GitHUb Link]() </br>
+[C9 Link]()
+
+## About this website
+
+The P is silent and so is this game. Its the same hand-held memory game from the 70s that we all know and love, but this is the PC version.
+Suitable for all ages to test their memory and skill without the click and lightup sounds.
+
+## UX
+
+__Ideal User__
+- Those who wants to play a simple computer game (for a change)
+- Those who want to keep their memory levels up there
+- Early childhood training
+- Senior adults memory excercise
+
+__How can they achieve this?__
+1. Click Start 
+2. Click Strict (Optional)
+3. Wait for computer's sequence to play
+4. Mirror computer's sequence by clicking on the colored buttons in the same order as the computer's sequence.
+5. 
+
+__Client stories__
 
 
-Hi there! Welcome to AWS Cloud9!
+## Features
 
-To get started, create some files, play with the terminal,
-or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
+-
+- 
+-
 
-Happy coding!
+
+## Wireframes
+
+
+<p align="center"><img src="https://user-images.githubusercontent.com/44424348/77587906-b2bb3680-6f4d-11ea-8786-c405c9ea38a9.JPG" /></p>
+
+<p align="center"><img src="https://user-images.githubusercontent.com/44424348/77587948-c6ff3380-6f4d-11ea-8494-c225734a040f.JPG" /></p>
+
+![psimonwindowview](https://user-images.githubusercontent.com/44424348/77587960-cb2b5100-6f4d-11ea-9694-0fdec6ff0622.JPG)
+
+
+
+
+#### Disclaimer
+
+The content of this website is for educational and milestone project purposes only.
--- assets/js/psimon.js
+++ assets/js/psimon.js
@@ -1,6 +1,7 @@
 //variables
-playerSequence = [];
-compSequence = [];
+playerSeq = [];
+compSeq = [];
+const NUMBER_OF_LEVELS = 20;
 var id, color, level = 0;
 
 $(document).ready(function() {
@@ -12,36 +13,79 @@ $(document).ready(function() {
     $(".button").click(function() {
         id= $(this).attr("id");
         color = $(this).attr("class").split(" ")[0];
+        playerSeq.push(id);
+        console.log(id+" "+color);
         addClass(id, color);
+        
+        if(!playerCorrect()) {
+            displayError();
+            playerSeq = [];
+            
+        }
+        // listening part
+        if(playerSeq.length == compSeq.length && playerSeq.length < NUMBER_OF_LEVELS) {
+            level++;
+            playerSeq = [];
+            compSequence();
+        }
+        
+        if(playerSeq.length == NUMBER_OF_LEVELS) {
+            $(".score").text(":)");
+        }
     });
     
     
 });
 
+//verifying player vs computer sequence
+function playerCorrect() {
+    for (var p = 0; p < playerSeq.length; p++) {
+        if(playerSeq[p] != compSeq[p]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-function compSequence () {
+//game over
+function displayError() {
+    console.log("error");
+    var counter = 0;
+    var myError = setInterval(function() {
+        $(".score").text(":(");
+        counter++;
+        if(counter == 3) {
+            $(".display").text(level);
+            clearInterval(myError);
+            playerSeq = [];
+            counter = 0;
+        }
+    }, 500);
+}
+
+
+
+function compSequence() {
     console.log(level);
     $(".score").text(level);
     getRandomNum();
     var p = 0;
     var myInterval = setInterval(function() {
-        id = compSequence[p];
+        id = compSeq[p];
         color = $("#"+id).attr("class").split(" ")[0];
         console.log(id+" "+color);
         addClass(id, color);
         p++;
-        if(p == compSequence.length) {
+        if(p == compSeq.length) {
           clearInterval(myInterval);
-          
         }
-        
     }, 1000);
 }
 
 
 function getRandomNum() {
     var random = Math.floor(Math.random() * 4);
-    compSequence.push(random);
+    compSeq.push(random);
 }
 
 function addClass(id, color) {
